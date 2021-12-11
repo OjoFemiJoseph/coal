dc2adbaf24085b7236995b7985a155c673dbb936
OjoFemiJoseph <70476696+OjoFemiJoseph@users.noreply.github.com>
2021-12-11 11:32:41 +0100

    Update layouts.blade (2).php

--- layouts.blade (2).php
+++ layouts.blade (2).php
@@ -586,14 +586,13 @@
         </div>
         <!-- end container_wrap-->
       </div>
-      <div class='header_bg'>
-      </div>
-      <!-- end header -->
-    </header>
-        
+      <di
+ 
+      <!end header -->
+    </header>      
 @yield('content')
 
-    <div id='main' class='all_colors' data-scroll-offset='0'>
+    <div id='main' clasll_colors' data-scroll-offset='0'>
       <div id='av_section_1' class='avia-section main_color avia-section-default avia-no-shadow avia-bg-style-scroll  avia-builder-el-0  avia-builder-el-no-sibling   container_wrap fullsize' style=' '>
 
       <div class='container'>
